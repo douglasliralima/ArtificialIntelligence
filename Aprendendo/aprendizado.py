e403d62a374e5ac2e82196f290611e8953c26498
douglasliralima <douglasliralima@gmail.com>
2018-05-23 12:21:32 -0300

    Adicionada uma funcao que rotuna com numeros coisas que antigamente eram strings

--- Aprendendo/aprendizado.py
+++ Aprendendo/aprendizado.py
@@ -17,25 +17,54 @@ from sklearn.decomposition import PCA
 from sklearn.metrics import r2_score
 from sklearn.svm import SVC
 
-def handle_non_numerical_data(df):
-    colunas = df.columns.values 		#Primeira coisa que nós vamos fazer é pegar o identificador de cada coluna, para transformar cada uma dela em valores numerais
-    for coluna in colunas: 				#Vamos pegar o valor de cada coluna
-        text_digit_vals = {}			#Criamos um dicionário que colocaremos os valores para cada coluna sistematizada
-        def convert_to_int(val):		#Aqui nós pegamos uma palavra chave
-            return text_digit_vals[val]	#retornamos então a coluna no dicionário com a coluna sistematizada caso o sistema precise
-        if df[coluna].dtype != np.int64 and df[coluna].dtype != np.float64:#Vemos se o valor naquela respectiva coluna 
-            column_contents = df[coluna].values.tolist()
-            unique_elements = set(column_contents)
-            x = 0
-            for unique in unique_elements:
-                if unique not in text_digit_vals:
-                    text_digit_vals[unique] = x
-                    x+=1
 
-            df[coluna] = list(map(convert_to_int, df[coluna]))
 
-    return df
+#A funcao pega todas as colunas que foram passadas no dataframe e discretiza os seus valores
+#retornando um dataframe com os valores discretizados
+def discretiza(colunas):
+	#Lembrando que ao recebermos um dicionário no for, ele vai printando suas palavras chave, então basta que
+	#a cada palavra chave no for, nós percorramos toda a linha
+	novasColunas = {}
+	for instancia in colunas:
+		palavras = []  #Representará cada um dos nossos rótulos
+		valores = {}   #Representará cada um dos valores númericos referentes a esses rótulos
+		valor = 0      #O número subirá a partir disso
+		rotulados = [] #Representacao de cada elemento classificado, com um rótulo numérico
 
+		for elemento in colunas[instancia]:
+			'''
+			Vou ver em uma lista se eu já não me deparei com esse
+			mesmo elemento anteriomente nessa coluna
+			'''
+			existe = False
+
+			for x in range(len(palavras)):
+				if(len(palavras) == 0):
+					#Se não existia nenhum elemento anteriormente, obviamente não havia nada antes para estar igual
+					existe = True
+					break
+				#Se eu encontrar um único elemento igual, eu retiro ela da minha lista
+				if(palavras[x] == elemento):
+					existe = True
+					palavras.remove(elemento)
+					#print("Dropei", elemento, end = "")
+					break
+			palavras.append(elemento)
+
+			#Bom, se o elemento não existir na minha lista, eu o adiciono no meu dicionário de valores
+			if(existe == False):
+				#print("Elemento:", elemento, "Valor:", valor)
+				valores[elemento] = valor
+				valor +=1
+			#Para cada instancia nessa coluna vamos pegar o valor correspondente do elemento, 
+			#em nosso dicionario de valores
+			rotulados.append(valores[elemento])
+		#Agora seguindo o formato do pandas, eu deixo a palavra chave daquela coluna, para a lista de rotulados
+		novasColunas[instancia] = rotulados
+	#Retornamos o dataframe convertido
+	return pd.DataFrame.from_dict(novasColunas) #Construir um dataframe a partir de um dicionario
+
+#----------------------------main----------------------------------------------------------------------------------
 #Primeira coisa que fazemos é carregar o nosso dataset em um objeto da classe DataFrame
 #Ele basicamente carrega o arquivo e coloca numa variável
 dados = pd.read_csv('Monstros.csv')
@@ -53,10 +82,10 @@ dados = pd.read_csv('Monstros.csv')
 dados = dados.drop(['ID', 'Sub-Type', 'Attack', 'Full Attack', 'Special Attacks', 'Special Qualities', 'Skills', 'Feats', 'Organization', 'Advancement'],
 	axis = 1)
 
-dados = handle_non_numerical_data(dados)
+dados = discretiza(dados)
+print(dados)
 
-print(dados.head()) #Veja, já é criado um ID automáticamente pelo negócio
-
+'''
 teste = {}
 print(type(teste))
 
@@ -86,4 +115,4 @@ arvore = DecisionTreeClassifier()
 arvore.fit(x_train, y_train)
 
 acuracia2 = arvore.score(x_test, y_test)
-print("Acuracia:", acuracia2)
+print("Acuracia:", acuracia2)'''
